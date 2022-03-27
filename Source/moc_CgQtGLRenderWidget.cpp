41f443127940402b53ea741cf7cd8f648e989fbb
“linux_joni” <lushaj2040@gmail.com>
2022-03-27 19:36:10 +0200

    ue01 afg 2

--- Source/moc_CgQtGLRenderWidget.cpp
+++ Source/moc_CgQtGLRenderWidget.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Source/CgQtViewer/CgQtGLRenderWidget.h"
+#include "CgQtViewer/CgQtGLRenderWidget.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
